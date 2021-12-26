8a807a306916de466e94e75ee8180a33dd33c8a4
bitworld75 <bitworld75@gmail.com>
2021-12-26 21:30:18 +0500

    Added Friend list Design

--- Web_A5/src/Components/profile/Profile.js
+++ Web_A5/src/Components/profile/Profile.js
@@ -1,7 +1,34 @@
 import React from 'react';
 import Navbar from '../homepage/navbar';
+import {
+  Card,
+  CardBody,
+  CardHeader,
+  Col,
+  Form,
+  FormGroup,
+  Input,
+  Label,
+  Row,
+} from 'reactstrap';
+import './Profile.css';
+import defaultUserImg from '../../assets/img/default_user.png';
 
 const Profile = () => {
+  const friendsList = [
+    {
+      name: 'Zulifaqr Ahmed',
+      email: 'zulifaqr@gmail.com',
+    },
+    {
+      name: 'Zulifaqr Ahmed',
+      email: 'zulifaqr@gmail.com',
+    },
+    {
+      name: 'Zulifaqr Ahmed',
+      email: 'zulifaqr@gmail.com',
+    },
+  ];
   return (
     <div
       style={{
@@ -14,14 +41,61 @@ const Profile = () => {
       }}
     >
       <Navbar />
-      <form>
-        <label>First Name</label>
-        <input type='text' name='first-name' placeholder='First Name' />
-        <label>Last Name</label>
-        <input type='text' name='first-name' placeholder='First Name' />
-        <label>Date of Birth</label>
-        <input type='date' name='first-name' placeholder='First Name' />
-      </form>
+      <Row>
+        <Col lg='4'>
+          {' '}
+          <Form className='user-form'>
+            <FormGroup>
+              <Label for='exampleEmail'>Email</Label>
+              <Input
+                type='email'
+                name='email'
+                id='exampleEmail'
+                placeholder='with a placeholder'
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for='examplePassword'>User Name</Label>
+              <Input
+                type='text'
+                name='password'
+                id='examplePassword'
+                placeholder='password placeholder'
+              />
+            </FormGroup>{' '}
+            <FormGroup>
+              <Label for='examplePassword'>Date of Birth</Label>
+              <Input
+                type='date'
+                name='password'
+                id='examplePassword'
+                placeholder='password placeholder'
+              />
+            </FormGroup>
+          </Form>
+        </Col>
+
+        <Col lg='8'>
+          <Card className='custom-card' style={{ width: 'min-content' }}>
+            <CardHeader>Friends List</CardHeader>
+            <CardBody className='custom-card-body'>
+              {friendsList?.map(({ name, email }) => (
+                <>
+                  <Card className='custom-card '>
+                    <CardHeader>{name}</CardHeader>
+                    <CardBody>
+                      <p>{email}</p>
+                      <center>
+                        <img src={defaultUserImg} alt='...' width='100' />
+                      </center>
+                    </CardBody>
+                  </Card>
+                </>
+              ))}
+            </CardBody>
+          </Card>
+        </Col>
+      </Row>
     </div>
   );
 };
